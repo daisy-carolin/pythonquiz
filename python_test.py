82365efbc7e2136fdcf6d41b03c601a84f266edd
student <daisy-carolin@gmail.com>
2021-06-17 10:51:20 +0300

    first commit

---
+++ python_test.py
@@ -0,0 +1,54 @@
+x = [100,110,120,130,140,150]
+multiplied_list=[element*5 for element in x]
+print(multiplied_list)
+
+def divisible_by_three():
+    x=range(10,100)
+    for y in x:
+        if y%3==0:
+            print(y)
+    
+
+x = [[1,2],[3,4],[5,6]], 
+flat_list = []
+for sublist in x:
+    for num in sublist:
+        flat_list.append(num)
+print(flat_list)
+
+    
+def smallest():
+    list1=[2,7,3,9,10]
+    list1.sort()
+print("Smallest element is:",(list1))
+
+def mylist():
+ x = ['a','b','a','e','d','b','c','e','f','g','h']
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
+
+
+def divisible_by_seven():
+    x=range(100,200)
+    for y in x:
+        if y%7==0:
+            print(y)
+
+
+def Students():
+    year_of_birth=2021-"age"
+    for student in Students:
+              [{"age": 19, "name": "Eunice"},
+              {"age": 21, "name": "Agnes"},
+              {"age": 18, "name": "Teresa"}, 
+              {"age": 22, "name": "Asha"} ]
+print("Hello {} you were born in year {}".format("name","year"))
+
+
+
+
+
+
+
+
+
